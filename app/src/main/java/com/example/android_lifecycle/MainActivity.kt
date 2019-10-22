90efb97ed0498b8eb3f88e310c7bf97546f76fe3
Angel Suarez <angsureaz@gmail.com>
2019-10-22 20:06:07 +0200

    Ejemplo de ciclo de vida de una Actividad

--- app/src/main/java/com/example/android_lifecycle/MainActivity.kt
+++ app/src/main/java/com/example/android_lifecycle/MainActivity.kt
@@ -2,11 +2,38 @@ package com.example.android_lifecycle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i("MainActivity","--> onCreate")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        Log.i("MainActivity","--> onStart")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Log.i("MainActivity","--> onResume")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.i("MainActivity","--> onPause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.i("MainActivity","--> onStop")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.i("MainActivity","--> onDestroy")
+        super.onDestroy()
     }
 }
